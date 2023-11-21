6f3524026b8842558149135af4194f4038163a14
Shaikh Mohammad Danish <65220642+SMD-1@users.noreply.github.com>
2023-11-21 23:09:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ submitBtn.addEventListener('click', function() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=be5e3aac9a59ebd3aa212105acead9bd&units=metric')
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             city.innerHTML = data['name'];
             temp.innerHTML = 'Tempreature: ' + data['main']['temp'] + '°C';
             desc.innerHTML = data['weather'][0]['description'];
